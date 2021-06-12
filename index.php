257888c54c7154da393871936a7dae73f7c7dfde
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-12 22:32:27 +0200

    add foreach

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once "./partials/template/header.php";
+
+require "./db/albums.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+
+    <?php
+
+    foreach ($albumsList as $albums) {
+
+    ?>
+
+
+        <ul>
+            <li>
+                <?php echo $albums["poster"]. "<br>" ?>
+                <strong><?php echo $albums["title"] ?></strong>
+                <p><?php echo $albums["author"] ?></p> 
+                <p><?php echo $albums["genre"] ?></p>
+                <p><?php echo $albums["year"] ?></p>
+            </li>
+        </ul>
+
+
+    <?php
+    }
+    ?>
+</body>
+
+</html>
